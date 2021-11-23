dcf21b478781b76bba4aee26e3b3322904b51dc8
Eduardo Diaz <lalo.diaz1896@gmail.com>
2021-11-22 19:31:32 -0600

    Add variables

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'clever-strapi.d746s.mongodb.net'),
+        host: env('DATABASE_HOST'),
         srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'clever-strapi'),
-        username: env('DATABASE_USERNAME', 'db_user'),
-        password: env('DATABASE_PASSWORD', 'abB36bVIxj952E1M'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
